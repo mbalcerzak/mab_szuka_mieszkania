aace1a1989d37a22d03f65dfcdc23ce3e3616c72
mbalcerzak <malgorzata.a.balcerzak@gmail.com>
2020-12-30 01:14:17 +0100

    reworked scraping script crawler crawls scraper scrapes life is good

--- src/scraping_gumtree.py
+++ src/scraping_gumtree.py
@@ -4,48 +4,50 @@ import json
 import re
 
 
+def get_description(soup):
+    results = soup.find(id="wrapper")
+    description = results.find('div', class_='description')
+
+    return description.text
 
 
-# przykładowy adres ogłoszenia:
-# ad_link = "https://www.gumtree.pl/a-mieszkania-i-domy-sprzedam-i-kupie/bielany/4-pokojowe-mieszkanie-bielany-ul-kochanowskiego/1008615130830911378860309"
+def get_photos(soup):
+    gallery = soup.find(id="vip-gallery-data")
+    pattern = re.compile('\{.*\}')
+    json_obj = json.loads(re.findall(pattern, str(gallery))[0])
+
+    return dict(zip(json_obj['alt-tags'], json_obj['large']))
+
+
+def get_attributes(soup):
+    results = soup.find(id="wrapper")
+    attributes = results.find('ul', class_='selMenu')
+    attributes2 = attributes.find_all('div', class_='attribute')
+
+    attr_dict = {}
+
+    for elem in attributes2:
+
+        attr_name = (elem.find('span', class_='name')).text
+        attr_val = (elem.find('span', class_='value')).text
+
+        if attr_name not in attr_dict:
+            attr_dict[attr_name] = attr_val
+
+    return attr_dict
+
+
+def get_flat_info(link):
+    page = requests.get(link)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    description = get_description(soup)
+    photos_links = get_photos(soup)
+    attributes = get_attributes(soup)
+    id = link.split('/')[-1]
+
+    return [id, attributes, description, photos_links]
+
 
 ad_link = 'https://www.gumtree.pl/a-mieszkania-i-domy-sprzedam-i-kupie/srodmiescie/mieszkanie-2-pokojowe-piekna-okolica/1006938050400911260981009'
-
-# ad_link = 'https://www.gumtree.pl/a-mieszkania-i-domy-sprzedam-i-kupie/rembertow/135m-%252Bogrodek-oddzielne-wejscie-5min-do-pkp-200m-do-autobusu-roi-10/1008614496810910556456609'
-
-page = requests.get(ad_link)
-soup = BeautifulSoup(page.content, 'html.parser')
-
-### description ##########################
-results = soup.find(id="wrapper")
-description = results.find('div', class_='description')
-print(description.text)
-
-
-#### photos ######################
-gallery = soup.find(id="vip-gallery-data")
-pattern = re.compile('\{.*\}')
-json_obj = json.loads(re.findall(pattern, str(gallery))[0])
-
-print(json_obj['large'])
-
-
-# #### - cenę mieszkania #############
-# - lokalizację
-# - wielkość mieszkania
-# - które piętro
-
-attributes = results.find('ul', class_='selMenu')
-attributes2 = attributes.find_all('div', class_='attribute')
-
-for elem in attributes2:
-
-    klucz = (elem.find('span', class_='name')).text
-    wartosc = (elem.find('span', class_='value')).text
-    print(klucz, wartosc)
-
-
-#### ID ogłoszenia
-print(ad_link.split('/')[-1])
-
-# class Mieszkanie:
+print(get_flat_info(ad_link))
